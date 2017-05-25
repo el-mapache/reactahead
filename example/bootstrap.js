dea51e561d298d73f3bbcf74fe2f54757d7588eb
Adam Biagianti <adam.biagianti@gsa.gov>
2017-05-24 22:41:10 -0400

    Testing loading data vi api callback

--- example/bootstrap.js
+++ example/bootstrap.js
@@ -2,5 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Typeahead from '../src';
 import pokemon from './data/pokemon';
+import defaultFilter from '../src/utils/generic-filter';
 
-ReactDOM.render(<Typeahead elements={pokemon} />, document.getElementById('app'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filteredElements: this.props.elements
+    };
+
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData(value, callback) {
+    const { elements } = this.props;
+
+    setTimeout(() => {
+      const filtered = defaultFilter(value, elements, 'name');
+
+      this.setState({
+        filteredElements: (filtered.length && filtered) || [ 'No results' ]
+      }, callback);
+    }, 150);
+  }
+
+  render() {
+    return (
+      <Typeahead
+        requestAsync={this.fetchData}
+        elements={this.state.filteredElements}
+      />
+    )
+  }
+}
+
+ReactDOM.render(<App elements={pokemon} />, document.getElementById('app'));
